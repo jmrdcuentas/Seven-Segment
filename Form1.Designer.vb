e520410faaac4d96af0d6a7e90386df2fa355701
jmrdcuentas <58081058+jmrdcuentas@users.noreply.github.com>
2020-03-11 17:57:24 +0800

    Add files via upload
    final

---
+++ Form1.Designer.vb
@@ -0,0 +1,148 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button0 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button0
+        '
+        Me.Button0.Location = New System.Drawing.Point(62, 25)
+        Me.Button0.Name = "Button0"
+        Me.Button0.Size = New System.Drawing.Size(175, 23)
+        Me.Button0.TabIndex = 0
+        Me.Button0.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(62, 193)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(175, 23)
+        Me.Button6.TabIndex = 1
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(62, 358)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(175, 23)
+        Me.Button3.TabIndex = 2
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(33, 25)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(23, 175)
+        Me.Button5.TabIndex = 3
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(33, 206)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(23, 175)
+        Me.Button4.TabIndex = 4
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(243, 206)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(23, 175)
+        Me.Button2.TabIndex = 5
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(243, 25)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(23, 175)
+        Me.Button1.TabIndex = 6
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(57, 399)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(180, 37)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "7 Segment"
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(62, 282)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(175, 23)
+        Me.Button7.TabIndex = 8
+        Me.Button7.UseVisualStyleBackColor = True
+        Me.Button7.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(316, 449)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button0)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button0 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Button7 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,42 @@
+﻿Public Class Form1
+    Dim Second As Int32 = -1
+    Dim format = New Integer(9, 6) {
+            {1, 1, 1, 1, 1, 1, 0},
+            {0, 1, 1, 0, 0, 0, 0},
+            {1, 1, 0, 1, 1, 0, 1},
+            {1, 1, 1, 1, 0, 0, 1},
+            {0, 1, 1, 0, 0, 1, 1},
+            {1, 0, 1, 1, 0, 1, 1},
+            {1, 0, 1, 1, 1, 1, 1},
+            {1, 1, 1, 0, 0, 0, 0},
+            {1, 1, 1, 1, 1, 1, 1},
+            {1, 1, 1, 1, 0, 1, 1}
+        }
+    Dim btns(6) As Button
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Button7.Focus()
+
+        For i As Integer = 0 To 6
+            btns(i) = CType(Me.Controls("Button" & i), Button)
+        Next
+
+        Timer1.Start()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Second = Second + 1
+
+        For ind As Integer = 0 To 6
+            If format(Second, ind) = 1 Then
+                btns(ind).BackColor = Color.Red
+            Else
+                btns(ind).BackColor = Color.White
+            End If
+        Next
+
+        If Second = 9 Then
+            Second = -1
+        End If
+    End Sub
+End Class
